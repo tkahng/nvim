8fbf810389993e592fc8eab3c9305232b7b60405
tkahng <tkahng@gmail.com>
2026-01-22 23:35:09 -0800

    add go-nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,6 +26,30 @@ return {
     },
   },
   {
+    "ray-x/go.nvim",
+    ft = { "go", "gomod", "gosum" },
+    dependencies = {
+      "mfussenegger/nvim-dap", -- Debug Adapter Protocol
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    -- config = true,
+    opts = {
+      -- verbose = true,
+      -- lsp_cfg = true,
+      -- gopls = {
+      --   settings = {
+      --     gopls = {
+      --       staticcheck = false,
+      --     },
+      --   },
+      -- },
+    },
+  },
+  {
     "mfussenegger/nvim-lint",
     opts = {
       linters_by_ft = {
