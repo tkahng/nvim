829d9b5f5bd760c95ce69917105fe83b006ce33d
tkahng <tkahng@gmail.com>
2026-02-10 14:28:39 -0800

    update plugins

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,11 +2,11 @@ return {
   {
     "folke/tokyonight.nvim",
     opts = {
-      -- transparent = true,
-      -- styles = {
-      --   sidebars = "transparent",
-      --   floats = "transparent",
-      -- },
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
 }
