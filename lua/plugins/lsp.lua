696cca4f370034b36a1bea82057c41d530fa7586
Tchunoo Rhee Kahng <tkahng@gmail.com>
2026-04-10 20:53:19 -0700

    Feature/go nvim (#1)
    * add go-nvim
    
    * feat: add surround
    
    * feat: update plugins
    
    * update plugins
    
    * feat: update plugins
    
    * feat: update lazy
    
    * feat: update specs
    
    * feat: update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,11 @@ return {
     ---@class PluginLspOpts
     opts = {
       servers = {
+        ["*"] = {
+          keys = {
+            { "K", vim.lsp.buf.hover, desc = "Hover" },
+          },
+        },
         gopls = {
           settings = {
             gopls = {
@@ -26,8 +31,40 @@ return {
     },
   },
   {
+    "ray-x/go.nvim",
+    ft = { "go", "gomod", "gosum" },
+    dependencies = {
+      "mfussenegger/nvim-dap", -- Debug Adapter Protocol
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    -- config = true,
+    opts = {
+      -- verbose = true,
+      -- lsp_cfg = true,
+      -- gopls = {
+      --   settings = {
+      --     gopls = {
+      --       staticcheck = false,
+      --     },
+      --   },
+      -- },
+    },
+  },
+  -- {
+  --   "nvim-java/nvim-java",
+  --   config = function()
+  --     require("java").setup()
+  --     vim.lsp.enable("jdtls")
+  --   end,
+  -- },
+  {
     "mfussenegger/nvim-lint",
     opts = {
+      events = { "BufWritePost", "BufReadPost", "InsertLeave" },
       linters_by_ft = {
         go = { "golangcilint" },
       },
